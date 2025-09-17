6d45eccbf3c66f54b883aa1617c421bc892da3da
Abraham Aigbokhan <abraham.aigbokhan@outlook.com>
2025-09-17 19:27:46 +0100

    Update urls.py

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from .views imporrt list_books , LibraryDetailView
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("relationship_app.urls")),
+    path("", include("relationship_app.urls")),   # ✅ include relationship_app routes
 ]
+
