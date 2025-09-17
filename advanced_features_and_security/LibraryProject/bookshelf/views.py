83f5bb7540e4d8cf1933474e5c3050b661e4d36d
Abraham Aigbokhan <abraham.aigbokhan@jeanedwards.com>
2025-09-17 20:59:47 +0100

    task 1 update

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book  # make sure Book model exists in bookshelf/models.py
+
+@permission_required('bookshelf.view_book', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()  # get all books from the DB
+    return render(request, 'bookshelf/book_list.html', {'books': books})
